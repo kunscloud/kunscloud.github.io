f0c84ce84abd0b97e939a04702ec2bca5bd0410b
kunscloud <100365805+kunscloud@users.noreply.github.com>
2022-04-16 23:41:05 +0800

    Add homework.js

---
+++ homework.js
@@ -0,0 +1,9 @@
+function writeAnswer(){
+ var response = prompt("What's the answer?");
+      if(response == "the red-haired boy"){
+        alert("Congratulations！You got it right！^-^");
+        }
+    else{
+        alert("Sorry, the correct answer is the red-haired boy~");
+     }
+}
