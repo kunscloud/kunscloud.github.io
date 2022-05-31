ceb99680d04147362d737f799a34eca2e7bd059e
kunscloud <100365805+kunscloud@users.noreply.github.com>
2022-05-31 23:42:06 +0800

    This is simple website about my idol

---
+++ NCT.js
@@ -0,0 +1,9 @@
+function writeAnswer(){
+ var response = prompt("请输入你的答案，中间用逗号间隔");
+      if(response == "李帝努，刘扬扬，李泰容，黄冠亨"){
+        alert("恭喜你答对了！看来认人对你来说一点也不难呢^-^");
+        }
+    else{
+        alert("很遗憾，答案是李帝努，刘扬扬，李泰容，黄冠亨");
+     }
+}
